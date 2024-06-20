9d92455dbbd3b9cc363035d42064cdba7cdeb9aa
Alex Cesar dos Santos <alecesar27@yahoo.com.br>
2024-06-20 14:40:29 -0300

    nova cor adicionada

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import {turboLogger} from './turboLogger.js'
 console.log("Estou usando o github");
 turboLogger("estou aprendendo o fluxo do git",'red');
+turboLogger("Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema","darkgreen");;
+
