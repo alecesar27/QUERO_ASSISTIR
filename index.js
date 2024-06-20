c235f9beac2d8a49fd37360ab2e22ef14b17ac07
Alex Cesar dos Santos <alecesar27@yahoo.com.br>
2024-06-20 15:11:43 -0300

    Conflito de Merge Resolvido

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import {turboLogger} from './turboLogger.js'
 console.log("Estou usando o github");
 turboLogger("estou aprendendo o fluxo do git",'red');
+turboLogger("Dev fez a atualização mudando a cor do texto","white");
